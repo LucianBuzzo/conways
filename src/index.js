b535386b69ba4aa80232003578e58d9ac9d9211b
Lucian <lucian.buzzo@gmail.com>
2021-01-01 16:23:31 +0000

    Initial commit
    Signed-off-by: Lucian <lucian.buzzo@gmail.com>

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const fabric = require('fabric')
+
+var canvas = new fabric.Canvas('c')
+
+// create a rectangle with angle=45
+var rect = new fabric.Rect({
+  left: 100,
+  top: 100,
+  fill: 'red',
+  width: 20,
+  height: 20,
+  angle: 45
+})
+
+canvas.add(rect)
